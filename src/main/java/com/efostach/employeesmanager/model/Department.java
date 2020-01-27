74ed034b1175e459157d51634b1ee0b12b16caa4
Helen Fostach <elena.fostach@gmail.com>
2020-01-27 23:20:32 +0200

    Added model layer

---
+++ src/main/java/com/efostach/employeesmanager/model/Department.java
@@ -0,0 +1,39 @@
+package com.efostach.employeesmanager.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "DEPARTMENTS")
+public class Department {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/efostach/employeesmanager/model/Employee.java
@@ -0,0 +1,100 @@
+package com.efostach.employeesmanager.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "EMPLOYEES")
+public class Employee {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "DATE_OF_BIRTH")
+    private Date dateOfBirth;
+
+    @Column(name = "SALARY")
+    private int salary;
+
+    @Column(name = "START_DATE")
+    private Date startDate;
+
+    @Column(name = "DEPARTMENT")
+    private int department;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", salary=" + salary +
+                ", startDate=" + startDate +
+                ", department=" + department +
+                '}';
+    }
+}
